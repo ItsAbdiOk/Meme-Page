35e5407135efb56b2917c01bd0c13926faecb875
Abdirahman Mohamed <mo.abdirahman99@gmail.com>
2023-05-23 18:32:51 +0100

    Making it ready for github pages

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def fetch_random_mountain_image():
         return "https://example.com/fallback_mountain_image.jpg"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug= False, host="0.0.0.0")
